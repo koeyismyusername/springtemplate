b770f1fa64bfec12630636b0ebb63d634438f2fb
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-22 14:33:08 +0900

    initial commit

---
+++ src/main/java/com/github/springtemplate/SpringtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.springtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/springtemplate/SpringtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
