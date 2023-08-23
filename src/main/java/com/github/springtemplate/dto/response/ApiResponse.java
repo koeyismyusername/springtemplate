be3340b83895100d789626cf1db6f56a90af645f
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-23 22:37:04 +0900

    Refactor: Swagger 작업

--- src/main/java/com/github/springtemplate/dto/response/ApiResponse.java
+++ src/main/java/com/github/springtemplate/dto/response/ApiResponse.java
@@ -1,11 +1,18 @@
 package com.github.springtemplate.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.io.Serializable;
 
+@Schema(name = "공통응답 폼")
 public abstract class ApiResponse<T> implements Serializable {
+    @Schema(name = "요청 성공 여부")
     private final boolean success;
+    @Schema(name = "응답 상태 코드")
     private final int status;
+    @Schema(name = "응답 메세지")
     private final String message;
+    @Schema(name = "응답 데이터")
     private final T data;
 
     public static <T> ApiResponse<T> success(String message, T data) {
--- src/main/java/com/github/springtemplate/dto/response/SignupResponse.java
+++ src/main/java/com/github/springtemplate/dto/response/SignupResponse.java
@@ -1,11 +1,16 @@
 package com.github.springtemplate.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(name = "회원가입 응답 객체")
 public record SignupResponse (
+        @Schema(name = "회원 id")
         int id,
+        @Schema(name = "회원 이메일")
         String email,
+        @Schema(name = "회원 이름")
         String name
 ){
 }
