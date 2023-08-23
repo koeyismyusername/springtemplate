b11aacbb07cb5c3b262d08107c78c17775c236a9
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-23 09:39:04 +0900

    Refactor: 추상 클래스로 구현, lombok 어노테이션 제거

--- src/main/java/com/github/springtemplate/util/ApiResponse.java
+++ src/main/java/com/github/springtemplate/util/ApiResponse.java
@@ -1,30 +1,45 @@
 package com.github.springtemplate.util;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
+public abstract class ApiResponse<T>{
+    private final boolean success;
+    private final int status;
+    private final String message;
+    private final T data;
 
-@Builder
-public record ApiResponse<T>(
-        boolean success,
-        int status,
-        String message,
-        T data
-) {
     public static <T> ApiResponse<T> success(String message, T data) {
-        return ApiResponse.<T>builder()
-                .success(true)
-                .status(200)
-                .message(message)
-                .data(data)
-                .build();
+        return new ApiResponse<T>(true, 200, message, data) {};
     }
 
     public static <T> ApiResponse<T> fail(int status, String message) {
-        return ApiResponse.<T>builder()
-                .success(false)
-                .status(status)
-                .message(message)
-                .data(null)
-                .build();
+        return new ApiResponse<T>(false, status, message, null) {};
+    }
+
+    public ApiResponse (boolean success, int status, String message, T data) {
+        this.success = success;
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+    public ApiResponse () {
+        this.success = false;
+        this.status = 500;
+        this.message = "이런 응답이 어떻게 해서 나오게되었는지 모르겠습니다. 전적으로 백엔드의 책임입니다.";
+        this.data = null;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
     }
 }
