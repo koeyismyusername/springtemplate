23ab7e41a792bd53d0e1a8f2bd1e345b5ac98b10
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 16:38:42 +0900

    Feat: 회원가입 구현

--- src/main/java/com/github/springtemplate/controller/UserController.java
+++ src/main/java/com/github/springtemplate/controller/UserController.java
@@ -3,23 +3,28 @@ package com.github.springtemplate.controller;
 import com.github.springtemplate.dto.request.SignupRequest;
 import com.github.springtemplate.dto.response.ApiResponse;
 import com.github.springtemplate.dto.response.SignupResponse;
+import com.github.springtemplate.dto.response.UserResponse;
+import com.github.springtemplate.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 @Tag(name = "회원가입, 로그인, 회원 정보 API")
 public class UserController {
 
+    private final UserService userService;
+
     @Operation(method = "GET", summary = "회원가입")
     @PostMapping("/auth/signup")
-    public ApiResponse<SignupResponse> signup(@RequestBody @Parameter(name = "회원가입 요청 객체", required = true) SignupRequest request) {
-        SignupResponse response = SignupResponse.builder().email(request.email()).id(1).name(request.name()).build();
-        return ApiResponse.success("회원가입에 성공했습니다.", response);
+    public ApiResponse<UserResponse> signup(@RequestBody @Parameter(name = "회원가입 요청 객체", required = true) SignupRequest request) {
+        return userService.signup(request);
     }
 }
