664350e027bf54db2fcdf761bee98a36a3dfc617
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 19:16:00 +0900

    Feat: 유효하지 않은 email로 유저정보 조회시 에러코드 추가

--- src/main/java/com/github/springtemplate/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/github/springtemplate/exception/errorcode/UserErrorCode.java
@@ -4,7 +4,8 @@ import com.github.springtemplate.exception.ApiException;
 import org.springframework.http.HttpStatus;
 
 public enum UserErrorCode implements ErrorCode {
-    EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 회원가입된 이메일입니다.");
+    EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 회원가입된 이메일입니다."), 
+    INVALID_EMAIL(HttpStatus.UNAUTHORIZED, "가입되지 않은 이메일입니다.");
 
     UserErrorCode(HttpStatus status, String message) {
         this.exception = new ApiException(status.value(), message) {};
