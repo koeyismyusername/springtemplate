d8681310ba1b32a7cd4a96d37eee5c95f5ae3aa8
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 16:08:18 +0900

    Feat: UserErrorCode 구현

---
+++ src/main/java/com/github/springtemplate/exception/errorcode/UserErrorCode.java
@@ -0,0 +1,18 @@
+package com.github.springtemplate.exception.errorcode;
+
+import com.github.springtemplate.exception.ApiException;
+import org.springframework.http.HttpStatus;
+
+public enum UserErrorCode implements ErrorCode {
+    EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 회원가입된 이메일입니다.");
+
+    UserErrorCode(HttpStatus status, String message) {
+        this.exception = new ApiException(status.value(), message) {};
+    }
+    private ApiException exception;
+
+    @Override
+    public ApiException getException() {
+        return exception;
+    }
+}
