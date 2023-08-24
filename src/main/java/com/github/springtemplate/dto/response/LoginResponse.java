c95afbedd89294d60980f78e0b2a791145b48c49
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 19:42:29 +0900

    Feat: from 메서드 추가

--- src/main/java/com/github/springtemplate/dto/response/LoginResponse.java
+++ src/main/java/com/github/springtemplate/dto/response/LoginResponse.java
@@ -1,7 +1,18 @@
 package com.github.springtemplate.dto.response;
 
+import com.github.springtemplate.dto.request.LoginRequest;
+import com.github.springtemplate.entity.Authorization;
+import lombok.Builder;
+
+@Builder
 public record LoginResponse(
         String token,
         String email
 ) {
+    public static LoginResponse from(Authorization savedData) {
+        return LoginResponse.builder()
+                .email(savedData.getEmail())
+                .token(savedData.getToken())
+                .build();
+    }
 }
