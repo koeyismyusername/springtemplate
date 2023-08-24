84e301e7ae170683a46a1ddfc6873fbccbd4e458
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 19:23:41 +0900

    Feat: 비밀번호 검사 에러코드 추가

--- src/main/java/com/github/springtemplate/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/github/springtemplate/exception/errorcode/UserErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
     EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 회원가입된 이메일입니다."), 
     INVALID_EMAIL(HttpStatus.UNAUTHORIZED, "가입되지 않은 이메일입니다."),
-    EXISTS_LOGIN(HttpStatus.CONFLICT, "이미 로그인되어 있습니다.");
+    EXISTS_LOGIN(HttpStatus.CONFLICT, "이미 로그인되어 있습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 옳지 않습니다.");
 
     UserErrorCode(HttpStatus status, String message) {
         this.exception = new ApiException(status.value(), message) {};
