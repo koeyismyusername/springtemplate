d9d6165869fa77c279c503bba46d53e4d937fde0
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 16:26:28 +0900

    Feat: ApiErrorCode 구현

---
+++ src/main/java/com/github/springtemplate/util/JpaUtils.java
@@ -0,0 +1,20 @@
+package com.github.springtemplate.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Slf4j
+public class JpaUtils {
+    public static <T, ID> T managedSave(JpaRepository<T, ID> repository, T entity) {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                return repository.save(entity);
+            } catch (OptimisticLockingFailureException e) {
+                log.warn("{} 저장 중에 충돌이 발생했습니다. {}회 시도", entity.getClass().getSimpleName(), i);
+            }
+        }
+
+        throw ApiErrorCode
+    }
+}
