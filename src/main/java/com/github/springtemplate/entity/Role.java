c406344426b3e3f03ff5b139287b3171bab22867
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-08-24 16:01:38 +0900

    Refactor: 간단 수정

--- src/main/java/com/github/springtemplate/entity/Role.java
+++ src/main/java/com/github/springtemplate/entity/Role.java
@@ -10,7 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Table(name = "role")
-@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode(of = "id", callSuper = false)
 public class Role {
 
     @Id
